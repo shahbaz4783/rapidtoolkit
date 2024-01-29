85c8a7989c19746d462f1a2701bedb7d8a6f6cdd
shahbaz4783 <shahbaz4783@gmail.com>
2024-01-29 21:22:09 +0530

    select option for base

--- src/layouts/base-convertor/BaseOptions.tsx
+++ src/layouts/base-convertor/BaseOptions.tsx
@@ -1,24 +1,45 @@
+import { FC } from 'react';
 import { Label } from '../../components/ui/Label';
 
-export const BaseOptions = () => {
+interface BaseOptions {
+	onHandleFromProp: any;
+	onHandleToProp: any;
+}
+
+export const BaseOptions: FC<BaseOptions> = ({
+	onHandleFromProp,
+	onHandleToProp,
+}) => {
 	return (
 		<>
 			<section className='flex '>
 				<div className='flex flex-1 flex-col gap-2'>
 					<Label htmlFor='from' title='From' />
-					<select name='' id='from' className='border p-2 rounded-s-lg '>
+					<select
+						id='from'
+						className='border p-2 rounded-s-lg'
+						onChange={onHandleFromProp}
+					>
 						<option value='2'>Binary</option>
 						<option value='8'>Octal</option>
-						<option value='10'>Decimal</option>
+						<option value='10' selected>
+							Decimal
+						</option>
 						<option value='16'>Hexadecimal</option>
 					</select>
 				</div>
 
 				<div className='flex flex-1 flex-col gap-2'>
 					<Label htmlFor='to' title='To' />
-					<select name='' id='to' className='border p-2 rounded-e-lg'>
+					<select
+						id='to'
+						className='border p-2 rounded-e-lg'
+						onChange={onHandleToProp}
+					>
 						<option value='2'>Binary</option>
-						<option value='16'>Hexadecimal</option>
+						<option value='16' selected>
+							Hexadecimal
+						</option>
 						<option value='8'>Octal</option>
 						<option value='10'>Decimal</option>
 					</select>
--- src/layouts/base-convertor/Form.tsx
+++ src/layouts/base-convertor/Form.tsx
@@ -4,21 +4,35 @@ import { useState, useRef } from 'react';
 import { BaseOptions } from "./BaseOptions";
 
 export const Form = () => {
+	const [fromBase, setfromBase] = useState(10);
+	const [toBase, settoBase] = useState(16);
 	const [enteredNum, setEnteredNum] = useState('');
+	
 	const number = useRef<HTMLInputElement | null>(null);
+
+	const handleFromOption = (event: any) => {
+		setfromBase(event.target.value);
+	};
+
+	const handleToOption = (event: any) => {
+		settoBase(event.target.value);
+	};
 	
 	const handleSubmit = (e: React.FormEvent) => {
 		e.preventDefault();
 		const userInput = number.current?.value;
-		const res = parseInt(userInput || '0', 10).toString(2);
+		const res = parseInt(userInput || '0', fromBase).toString(toBase);
 		setEnteredNum(res);
 	};
 
 	return (
 		<>
-			<form onSubmit={handleSubmit} className="flex flex-col gap-10 w-2/3">
-				<BaseOptions />
-				<Input placeholder='Enter a Number' ref={number} required/>
+			<form onSubmit={handleSubmit} className='flex flex-col gap-10 w-2/3'>
+				<BaseOptions
+					onHandleFromProp={handleFromOption}
+					onHandleToProp={handleToOption}
+				/>
+				<Input placeholder='Enter a Number' ref={number} required />
 				<Button title='Convert' />
 			</form>
 			<h1 className='font-serif text-3xl text-center'>{enteredNum}</h1>
